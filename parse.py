cba5c9891b5f1b8439c1ae793256f5253a0267b8
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-04-27 23:50:50 +0200

    print all attributes in msg

--- parse.py
+++ parse.py
@@ -7,8 +7,11 @@ for message_key, message in maildir.items():
     subject = message['subject']
     sender = message['from']
     date = message['date']
+    for header, value in message.items():
+        print(f"{header}: {value}")
+    break
 
-    print(f'Subject: {subject}')
-    print(f'From: {sender}')
-    print(f'Date: {date}')
-    print('-' * 40)
+    # print(f'Subject: {subject}')
+    # print(f'From: {sender}')
+    # print(f'Date: {date}')
+    # print('-' * 40)
