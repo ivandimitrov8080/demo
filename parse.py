e22068a3c6087da0896711c9bb3e1e9017423634
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-04-27 23:35:05 +0200

    parse emails

---
+++ parse.py
@@ -0,0 +1,14 @@
+import mailbox
+import email
+
+maildir = mailbox.Maildir("/home/ivand/mail/ivan/INBOX")
+
+for message_key, message in maildir.items():
+    subject = message['subject']
+    sender = message['from']
+    date = message['date']
+
+    print(f'Subject: {subject}')
+    print(f'From: {sender}')
+    print(f'Date: {date}')
+    print('-' * 40)
