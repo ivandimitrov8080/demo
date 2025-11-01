400513cf323025469a44ac8cc66f00769bc443e8
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-11-01 18:45:04 +0100

    balls

--- Main.java
+++ Main.java
@@ -1,47 +1,34 @@
-import java.util.Arrays;
-
 public class Main {
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(args));
-    // copilot r u here
-    System.out.println("Hello, World!");
+    for (int i = 0; i < 100; i++) {
+      var b = new Ball("red", 5.0);
+      System.out.println(b);
+    }
   }
 }
 
-// TODO: Implement a very simple stack
+class Ball {
+  private String color;
+  private double radius;
 
-class SimpleStack<T> {
-  private Object[] elements;
-  private int size;
-  private static final int DEFAULT_CAPACITY = 10;
-
-  public SimpleStack() {
-    elements = new Object[DEFAULT_CAPACITY];
-    size = 0;
+  public Ball(String color, double radius) {
+    this.color = color;
+    this.radius = radius;
   }
 
-  public void push(T item) {
-    if (size == elements.length) {
-      resize();
-    }
-    elements[size++] = item;
+  public String getColor() {
+    return color;
   }
 
-  public T pop() {
-    if (isEmpty()) {
-      throw new IllegalStateException("Stack is empty");
-    }
-    T item = (T) elements[--size];
-    elements[size] = null; // Avoid memory leak
-    return item;
+  public double getRadius() {
+    return radius;
   }
 
-  public boolean isEmpty() {
-    return size == 0;
-  }
-
-  private void resize() {
-    int newCapacity = elements.length * 2;
-    elements = Arrays.copyOf(elements, newCapacity);
+  @Override
+  public String toString() {
+    return "Ball{" +
+        "color='" + color + '\'' +
+        ", radius=" + radius +
+        '}';
   }
 }
