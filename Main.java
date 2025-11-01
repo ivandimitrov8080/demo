7e798e278fe6a79ae2e693301ffbbdcf8ee85242
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-11-01 19:28:22 +0100

    hw

--- Main.java
+++ Main.java
@@ -1,34 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    for (int i = 0; i < 100; i++) {
-      var b = new Ball("red", 5.0);
-      System.out.println(b);
-    }
-  }
-}
-
-class Ball {
-  private String color;
-  private double radius;
-
-  public Ball(String color, double radius) {
-    this.color = color;
-    this.radius = radius;
-  }
-
-  public String getColor() {
-    return color;
-  }
-
-  public double getRadius() {
-    return radius;
-  }
-
-  @Override
-  public String toString() {
-    return "Ball{" +
-        "color='" + color + '\'' +
-        ", radius=" + radius +
-        '}';
+    System.out.println("Hello, World!");
   }
 }
