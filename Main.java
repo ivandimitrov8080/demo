b0d63d59541a4f8ccc613ede13f4fe276888e3af
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-11-02 01:32:25 +0100

    whatever

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
     System.out.println(fib(200));
+    System.out.println(fibRec(20));
   }
 
   /*
@@ -20,5 +20,26 @@ public class Main {
       res.add(c);
     }
     return res;
+  }
+
+  // fib but recursive and make it show all numbers in array
+  public static List<Integer> fibRec(int n) {
+    List<Integer> res = new ArrayList<>();
+    fibRecHelper(n, res);
+    return res;
+  }
+
+  private static void fibRecHelper(int n, List<Integer> res) {
+    if (n <= 0) {
+      return;
+    }
+    if (n == 1) {
+      res.add(1);
+      return;
+    }
+    fibRecHelper(n - 1, res);
+    int nextFib = (res.size() >= 2) ? res.get(res.size() - 1) + res.get(res.size() - 2) : 1;
+    res.add(nextFib);
+
   }
 }
