b76ef01dcb50349f4debd52d1160aa34b7ac1cd2
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-11-02 17:56:34 +0100

    flake with nvim

--- flake.nix
+++ flake.nix
@@ -1,12 +1,38 @@
 {
   inputs = {
+    # nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # preconfigured defaults
+    configuration.url = "github:ivandimitrov8080/configuration.nix";
+    # nvim config helper - needed to work with configuration ^
+    nixvim-flake.url = "github:nix-community/nixvim";
+    nixvim-flake.inputs.nixpkgs.follows = "nixpkgs";
+    # neovim latest version
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      configuration,
+      nixvim-flake,
+      neovim-nightly-overlay,
+    }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      inherit (nixvim-flake.legacyPackages.${system}) makeNixvim;
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: {
+            nixvim = makeNixvim {
+              package = neovim-nightly-overlay.packages.${system}.default;
+            };
+          })
+          configuration.overlays.default
+        ];
+      };
     in
     {
       packages.${system} = {
@@ -15,7 +41,13 @@
       };
       devShells.${system} = {
         default = pkgs.mkShell {
-          buildInputs = with pkgs; [ go ];
+          buildInputs = with pkgs; [
+            go
+            (nixvim.main.extend {
+              lsp.servers.gopls.enable = true;
+              plugins.dap-go.enable = true;
+            })
+          ];
         };
       };
       checks.${system} = {
